96a8c35866849b1c3f4ea3813d182c5cc4451e58
Ramon Voges <kontakt@ramonvoges.de>
2019-09-03 17:31:25 +0200

    ID

--- wikidata.py
+++ wikidata.py
@@ -3,6 +3,7 @@ import re
 
 
 from urllib.request import urlopen
+import urllib.parse
 from bs4 import BeautifulSoup
 
 import pandas as pd
@@ -13,10 +14,9 @@ from geopy.extra.rate_limiter import RateLimiter
 import plotly.express as px
 
 
-base_url = "https://de.wikipedia.org"
-what = "/wiki/"
+base_url = "https://de.wikipedia.org/wiki/"
 term = "17._September"
-search = base_url + what + term
+search = base_url + term
 
 html = urlopen(search)
 
@@ -32,38 +32,38 @@ data = []
 
 for link in links:
     person = {}
-    name = re.sub("_", " ", link)
-    name = re.sub("%C3%BC", "ü", name)
-    name = re.sub("%C3%B6", "ö", name)
-    name = re.sub("%C3%A4", "ä", name)
-    name = re.sub("%E2%80%93", "–", name)
-    person['name'] = name
-    person['wiki_link'] = base_url + what + link
-    try:
-        page = wptools.page(name, lang='de')
-        page.get_wikidata()
-        person['wiki_data'] = page.data.get('wikidata')
-        if page.data.get('wikidata').get('Geburtsort (P19)'):
-            place_of_birth = page.data.get('wikidata').get('Geburtsort (P19)')
-            person['place_of_birth'] = re.sub(r"\(Q[0-9]*\)", "", place_of_birth)
-            # print("Geboren: {}".format(place_of_birth))
-        else:
-            # print('Keine Angabe zum Geburtsort')
-            person['place_of_birth'] = None
-        if page.data.get('wikidata').get('Sterbeort (P20)'):
-            place_of_death = page.data.get('wikidata').get('Sterbeort (P20)')
-            person['place_of_death'] = re.sub(r"\(Q[0-9]*\)", "", place_of_death)
-            # print("Gestorben: {}".format(place_of_death))
-        else:
-            # print('Keine Angabe zum Sterbeort')
-            person['place_of_death'] = None        
-    except:
-        # print('Abfragefehler')
-        person['wiki_data'] = None
+    # name = re.sub("_", " ", link)
+    # name = re.sub("%C3%BC", "ü", name)
+    # name = re.sub("%C3%B6", "ö", name)
+    # name = re.sub("%C3%A4", "ä", name)
+    # name = re.sub("%E2%80%93", "–", name)
+    person['name'] = urllib.parse.unquote(link)
+    person['wiki_link'] = base_url + link
+    html_person = urlopen(person['wiki_link'])
+    soup_person = BeautifulSoup(html_person, 'html.parser')
+    wikidata_link = soup_person.find('li', {'id': 't-wikibase'}).find('a')['href']
+    print(wikidata_link)
+    pattern = re.compile('Q\d+')
+    id = pattern.search(wikidata_link).group()
+    print(id)
+    page = wptools.page(wikibase=id, lang='de')
+    page.get_wikidata()
+    # person['wiki_data'] = page.data.get('wikidata')
+    if page.data.get('wikidata').get('Geburtsort (P19)'):
+        place_of_birth = page.data.get('wikidata').get('Geburtsort (P19)')
+        person['place_of_birth'] = re.sub(r"\(Q[0-9]*\)", "", place_of_birth)
+        # print("Geboren: {}".format(place_of_birth))
+    else:
+        # print('Keine Angabe zum Geburtsort')
         person['place_of_birth'] = None
-        person['place_of_death'] = None
-    finally:
-        data.append(person)
+    if page.data.get('wikidata').get('Sterbeort (P20)'):
+        place_of_death = page.data.get('wikidata').get('Sterbeort (P20)')
+        person['place_of_death'] = re.sub(r"\(Q[0-9]*\)", "", place_of_death)
+        # print("Gestorben: {}".format(place_of_death))
+    else:
+        # print('Keine Angabe zum Sterbeort')
+        person['place_of_death'] = None        
+    data.append(person)
 
 # for d in data:
 #     print(d)
